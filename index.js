58d2bfc9f6dccfc6f20631bdfd0a788da48df493
John Mook <jedwardmook@yahoo.com>
2022-01-10 15:53:57 -0600

    commit tree aec375832ce2589543f731dc493bd6975983d358

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  if (someNumber < 401){
+  return 'This one is on me!'
+  } else if (someNumber < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(checkCity){
   // Write your code here!
+  return checkCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(amount){
   // Write your code here!
+  switch(amount){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default :
+      return "Bye."
+  }
 }
